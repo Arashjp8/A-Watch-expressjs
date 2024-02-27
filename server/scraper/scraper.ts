322e465a99c2949842ae42a331416246150ec4cd
arashjp8 <arashjafarpour723@gmail.com>
2024-02-27 14:29:56 +0330

    [Refactor] - Making a movieObj

--- server/scraper/scraper.ts
+++ server/scraper/scraper.ts
@@ -14,8 +14,11 @@ import { parseDateAndLanguage } from "../utils/dateAndLanguageUtils";
 
 const instance = axios.create({ baseURL: "https://www.themoviedb.org" });
 
+let page = 1;
+const totalPages = 5;
+
 try {
-  instance.get("/movie").then(async (response) => {
+  instance.get(`/movie?page=${page}`).then(async (response) => {
     const $ = cheerio.load(response.data);
     const movies = [];
 
@@ -37,37 +40,39 @@ try {
             .attr("data-percent")!
             .trim();
           const overview = movie$(overviewCssPath).text().trim();
-          const crew = movie$(crewCssPath).text().trim();
-          const cast = movie$(
-            "body.en.v4 div.page_wrap.movie_wrap main#main.smaller.subtle section.inner_content.movie_content.backdrop.poster div#media_v4.media.movie_v4.header_large div.column_wrapper div.content_wrapper div div.white_column section.panel.top_billed.scroller div#cast_scroller.scroller_wrap.should_fade.is_fading ol.people.scroller",
-          )
-            .text()
-            .trim();
-          const genres = movie$(genresCssPath).text().trim();
+          const crew = organizeCastAndCrew(movie$(crewCssPath).text().trim());
+          const cast = organizeCastAndCrew(
+            movie$(
+              "body.en.v4 div.page_wrap.movie_wrap main#main.smaller.subtle section.inner_content.movie_content.backdrop.poster div#media_v4.media.movie_v4.header_large div.column_wrapper div.content_wrapper div div.white_column section.panel.top_billed.scroller div#cast_scroller.scroller_wrap.should_fade.is_fading ol.people.scroller",
+            )
+              .text()
+              .trim(),
+          );
+          const genres = parseGenres(movie$(genresCssPath).text().trim());
           const posterPath = movie$("img.poster").attr("src");
           const backdrop = movie$("img.backdrop").attr("src");
 
+          const movie = {
+            title,
+            voteAverage,
+            overview,
+            posterPath,
+            backdrop,
+            releaseDate,
+            originalLanguage,
+            genres,
+            cast,
+            crew,
+          };
+          movies.push(movie);
+
           console.log("\n=====================");
-          console.log("\nTitle: " + title);
-          console.log("\nVote Average: " + voteAverage);
-          console.log("\nOverview: " + overview);
 
-          console.log("\nPoster path: " + posterPath);
-          console.log("\nBackdrops: " + backdrop);
-
-          console.log("\nGenre: ");
-          console.log(parseGenres(genres));
-
-          console.log("\nRelease Date: " + releaseDate);
-          console.log("\noriginal Language: " + originalLanguage);
-
-          console.log("\nCrew: ");
-          console.log(organizeCastAndCrew(crew));
-
-          console.log("\nCast: ");
-          console.log(organizeCastAndCrew(cast));
+          console.log(movie);
 
           console.log("=====================\n");
+          console.log(movies.length);
+          console.log("page: " + page);
         })
         .catch((error) => {
           console.error("Error fetching movie data:", error);
