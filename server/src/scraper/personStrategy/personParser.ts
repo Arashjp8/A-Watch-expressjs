6919827e60ff53303c805411e4b5ed7966f7e17e
arashjp8 <arashjafarpour723@gmail.com>
2024-05-09 07:59:19 +0330

    [refactor] - personParser error handling refactored, using axios then() catch() blocks

--- server/src/scraper/personStrategy/personParser.ts
+++ server/src/scraper/personStrategy/personParser.ts
@@ -24,75 +24,92 @@ export const personParser = async (personLinks: PersonLinksObject) => {
 const scrapeData = async (link: string) => {
   console.log("scraping ", link);
 
-  try {
-    const response = await axiosInstance.get(link);
-    const $ = cheerio.load(response.data);
+  return axiosInstance
+    .get(link)
+    .then(async (response) => {
+      const $ = cheerio.load(response.data);
 
-    const id = getIDFromLink(link);
-    const name = $("div.title h2.title a").text();
-    const biography = $(
-      "section.full_wrapper div.biography div.content div.text p",
-    ).text();
-    const knowForDepartment = $("section.facts p:contains(Known For)")
-      .text()
-      .replace("Known For ", "")
-      .trim();
-    const placeOfBirth = $("section.facts p:contains(Place of Birth)")
-      .text()
-      .replace("Place of Birth ", "")
-      .trim();
-    const gender =
-      $("section.facts p:contains(Gender)")
+      const id = getIDFromLink(link);
+      const name = $("div.title h2.title a").text();
+      const biography = $(
+        "section.full_wrapper div.biography div.content div.text p",
+      ).text();
+      const knowForDepartment = $("section.facts p:contains(Known For)")
         .text()
-        .replace("Gender ", "")
+        .replace("Known For ", "")
+        .trim();
+      const placeOfBirth = $("section.facts p:contains(Place of Birth)")
+        .text()
+        .replace("Place of Birth ", "")
+        .trim();
+      const gender =
+        $("section.facts p:contains(Gender)")
+          .text()
+          .replace("Gender ", "")
+          .trim()
+          .toUpperCase() === "FEMALE"
+          ? 1
+          : 2;
+      const birthday = $("section.facts p:contains(Birthday)")
+        .text()
+        .replace("\n", "")
         .trim()
-        .toUpperCase() === "FEMALE"
-        ? 1
-        : 2;
-    const birthday = $("section.facts p:contains(Birthday)")
-      .text()
-      .replace("\n", "")
-      .trim()
-      .replace("Birthday\n", "")
-      .trim()
-      .replace(/ \(.*?\)/, "")
-      .trim();
-    const profilePath = $("section.images.inner div div img").attr("src");
+        .replace("Birthday\n", "")
+        .trim()
+        .replace(/ \(.*?\)/, "")
+        .trim();
+      const profilePath = $("section.images.inner div div img").attr("src");
 
-    const movieIDs = await getFilmography(link, "movie");
-    const tvShowIDs = await getFilmography(link, "tv");
+      const getFilmographyPromises = [
+        await getFilmography(link, "movie"),
+        await getFilmography(link, "tv"),
+      ];
 
-    return {
-      id,
-      name,
-      biography,
-      gender,
-      birthday,
-      knowForDepartment,
-      placeOfBirth,
-      profilePath,
-      movieIDs,
-      tvShowIDs,
-    };
-  } catch (error: any) {
-    console.error(`Error scraping ${link}: ${error.message}`);
-    throw error;
-  }
+      return Promise.all(getFilmographyPromises).then(
+        ([movieIDs, tvShowIDs]) => {
+          return {
+            id,
+            name,
+            biography,
+            gender,
+            birthday,
+            knowForDepartment,
+            placeOfBirth,
+            profilePath,
+            movieIDs,
+            tvShowIDs,
+          };
+        },
+      );
+    })
+    .catch((error) => {
+      console.error(`Error scraping ${link}: ${error.message}`);
+      throw error;
+    });
 };
 
-const getFilmography = async (link: string, creditMediaType: string) => {
-  let allFilmIDs: string[] = [];
+const getFilmography = async (
+  link: string,
+  creditMediaType: string,
+): Promise<string[]> => {
+  console.log("Getting filmography for:", link);
 
-  const response = await axiosInstance.get(
-    `${link}?credit_media_type=${creditMediaType}`,
-  );
-  const $ = cheerio.load(response.data);
+  return axiosInstance
+    .get(`${link}?credit_media_type=${creditMediaType}`)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      let allFilmIDs: string[] = [];
 
-  $("table.card.credits table.credit_group tr").each((_, element) => {
-    const filmLink = $(element).find("a.tooltip").attr("href");
-    const filmID = filmLink ? getIDFromLink(filmLink) : null;
-    filmID ? allFilmIDs.push(filmID) : null;
-  });
+      $("table.card.credits table.credit_group tr").each((_, element) => {
+        const filmLink = $(element).find("a.tooltip").attr("href");
+        const filmID = filmLink ? getIDFromLink(filmLink) : null;
+        filmID ? allFilmIDs.push(filmID) : null;
+      });
 
-  return allFilmIDs;
+      return allFilmIDs;
+    })
+    .catch((error) => {
+      console.error(`Error getting filmography for ${link}: ${error.message}`);
+      throw error;
+    });
 };
