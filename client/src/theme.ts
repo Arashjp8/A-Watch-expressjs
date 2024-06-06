62f1d8003396d6d796951c8e305fcec434f1af2f
arashjp8 <arashjafarpour723@gmail.com>
2024-06-06 18:01:32 +0330

    [checkpoint] - setting up the client side

---
+++ client/src/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({
+  fonts: {},
+  colors: {},
+  config,
+});
+export default theme;
