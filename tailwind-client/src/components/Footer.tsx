0f5be348a7aae0292dd3c9611242cc85dff6b985
arashjp8 <arashjafarpour723@gmail.com>
2024-06-07 17:46:09 +0330

    [checkpoint] - footer added

--- tailwind-client/src/components/Footer.tsx
+++ tailwind-client/src/components/Footer.tsx
@@ -1,7 +1,21 @@
 function Footer() {
   return (
-    <div className={"basis-1/12 justify-end"}>
-      <h1 className="text-3xl font-bold underline">footer</h1>
+    <div
+      className={
+        "basis-1/12 flex justify-center items-center border border-lime-500 w-[100vw] h-[80px] text-center"
+      }
+    >
+      <p>
+        Copyright © 2024{" "}
+        <a
+          href="https://twitter.com/arashjafarpour1"
+          target="blank"
+          className="hover:text-blue-600 hover:border-b-2 border-blue-600"
+        >
+          @arashjafarpour1
+        </a>
+        . All Rights Reserved{" "}
+      </p>
     </div>
   );
 }
