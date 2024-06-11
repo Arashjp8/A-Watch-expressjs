8f8147e298f790c47eb14f8ee4713344d9812bdf
arashjp8 <arashjafarpour723@gmail.com>
2024-06-11 07:57:58 +0330

    [checkpoint] - better tailwind breakpoints

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -5,21 +5,23 @@ function MovieCard() {
     <>
       <div
         className={
-          "p-8 grid grid-cols-5 grid-rows-4 gap-4 w-full h-full border border-lime-700 text-gray-600"
+          "p-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 grid-rows-4 gap-4 w-full h-full border border-lime-700 text-gray-600"
         }
       >
         {movies.map((movie) => (
           <div
             key={movie._id}
             className={
-              "h-48 flex flex-row border bg-gray-100 rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-300 ease-in-out hover:text-black"
+              "lg:h-48 flex flex-col lg:flex-row border bg-gray-100 rounded-md hover:scale-105 hover:cursor-pointer transition-all duration-300 ease-in-out hover:text-black"
             }
             onClick={() => console.log(movie.poster_path)}
           >
             <img
               src={movie.poster_path}
               loading={"lazy"}
-              className={"w-32 h-48 rounded-l-md"}
+              className={
+                "md:w-full md:h-[432px] lg:w-32 lg:h-48 rounded-md lg:rounded-l-md"
+              }
             />
             <div
               className={"flex flex-col gap-4 p-4 items-start justify-start"}
@@ -29,21 +31,21 @@ function MovieCard() {
                   ? movie.title.slice(0, 40) + "..."
                   : movie.title}
               </span>
+              <span className={"text-lg"}>{movie.release_date}</span>
               <div className={"flex flex-row gap-6"}>
-                <span>{movie.release_date}</span>
                 <span
-                  className={`${movie.vote_average < 50 ? "bg-red-200 text-red-700" : movie.vote_average < 70 ? "bg-yellow-200 text-yellow-700" : "bg-green-200 text-green-700"} py-1 px-2 rounded-md min-w-4`}
+                  className={`${movie.vote_average < 50 ? "bg-red-200 text-red-700" : movie.vote_average < 70 ? "bg-yellow-200 text-yellow-700" : "bg-green-200 text-green-700"} py-1 px-2 rounded-md text-center min-w-9`}
                 >
                   {movie.vote_average}
                 </span>
+                <button
+                  className={
+                    "bg-teal-200 text-md px-2 pb-1 flex flex-row items-center justify-center text-center rounded-lg hover:bg-opacity-70 transition-all duration-300 ease-in-out"
+                  }
+                >
+                  more
+                </button>
               </div>
-              <button
-                className={
-                  "bg-teal-400 px-2 py-1 flex items-center justify-center rounded-md hover:bg-opacity-70 transition-all duration-300 ease-in-out"
-                }
-              >
-                more
-              </button>
             </div>
           </div>
         ))}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     fontFamily: {
       lato: ["Lato", "sans-serif"],
     },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1200px",
+      xl: "1470px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
