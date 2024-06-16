d0c81222a83aa55247c1e6b9f83f3d827014da2f
arashjp8 <arashjafarpour723@gmail.com>
2024-06-16 09:27:43 +0330

    [checkpoint] - trying to build react-query from scratch

---
+++ client/src/hooks/useQuery.ts
@@ -0,0 +1,61 @@
+// most important things we need
+// 1. geting the queryKey and queryFn
+// 2. set a state based on the QueryState<T> interface
+// 3. write a fetchData function that checks the cache data then trys fetching with queryFn
+// 4. returning the state based on the result of the try catch block
+// 5. run the fetchData with a useEffect every time the queryKey changed
+
+import { useEffect, useState } from "react";
+
+interface QueryState<T> {
+  data: T | null;
+  error: Error | null;
+  isLoading: boolean;
+  isFetching: boolean;
+}
+
+const cache: { [key: string]: any } = {};
+
+const useQuery = <T>(queryKey: string, queryFn: () => Promise<T>) => {
+  const [state, setState] = useState<QueryState<T>>({
+    data: null,
+    error: null,
+    isLoading: false,
+    isFetching: false,
+  });
+
+  const fetchData = async () => {
+    if (cache[queryKey]) {
+      setState({
+        data: cache[queryKey],
+        error: null,
+        isLoading: false,
+        isFetching: false,
+      });
+      return;
+    }
+
+    setState((prev) => ({ ...prev, isLoading: true, isFetching: true }));
+
+    try {
+      const data = await queryFn();
+      cache[queryKey] = data;
+      setState({ data, error: null, isLoading: false, isFetching: false });
+    } catch (err) {
+      setState({
+        data: null,
+        error: err as Error,
+        isLoading: false,
+        isFetching: false,
+      });
+    }
+
+    useEffect(() => {
+      fetchData();
+    }, [queryKey]);
+
+    return state;
+  };
+};
+
+export default useQuery;
