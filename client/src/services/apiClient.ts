7ff7f035f2fbfdd38de06bee92d444f99a5b5c21
arashjp8 <arashjafarpour723@gmail.com>
2024-06-25 14:06:35 +0330

    [fix] - apiClient not recognizing the token bug

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -2,28 +2,35 @@ export async function apiClient(endpoint: string, method: string, body?: any) {
   const baseURL = `${import.meta.env.VITE_REACT_APP_LOCALBASEURL}/api`;
   const url = `${baseURL}${endpoint}`;
 
-  const user = JSON.parse(localStorage.getItem("user") || "null");
-  const token = user.token;
-
   try {
-    const options: RequestInit = {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    const token = user.token;
+
+    let options: RequestInit = {
       method: method,
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
       },
       credentials: "include",
       body: body ? JSON.stringify(body) : undefined,
     };
 
+    if (token) {
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${token}`,
+      };
+    }
     const response = await fetch(url, options);
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    return response.json();
+    const json = await response.json();
+    return json;
   } catch (err) {
-    console.error("Error while fetching data: ", err);
+    console.error("Error while fetching data:", err);
+    throw err;
   }
 }
